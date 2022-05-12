a86d362796bb98430513ecb53aab57a49589f1ff
Diego Gomes <diego.gomes.191287@gmail.com>
2022-05-12 15:05:33 -0300

    adding dockerfile and docker-compose

--- Users.API/Program.cs
+++ Users.API/Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddDbContext<UserDbContext>(options =>
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
